cc0a4f967f48165eb08ca26c1aace3bd333187ae
David Lanier <dlanier@phase2technology.com>
2023-11-14 14:22:18 -0800

    Track click events

--- src/components/Cta.tsx
+++ src/components/Cta.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import { twMerge } from "tailwind-merge";
+import { Link } from "@yext/pages/components";
 
 export interface CtaProps {
   buttonText: string;
@@ -9,19 +10,18 @@ export interface CtaProps {
 
 const Cta = ({ buttonText, url, style }: CtaProps) => {
   return (
-    <>
-      <a
-        href={url}
-        className={twMerge(
-          "py-4 px-6 text-base font-bold rounded-lg hover:scale-[1.02] duration-250",
-          style
-        )}
-        target=""
-        rel="noopener noreferrer"
-      >
-        {buttonText}
-      </a>
-    </>
+    <Link
+      href={url}
+      className={twMerge(
+        "py-4 px-6 text-base font-bold rounded-lg hover:scale-[1.02] duration-250",
+        style
+      )}
+      target=""
+      rel="noopener noreferrer"
+      eventName={`cta Click: ${buttonText}`}
+    >
+      {buttonText}
+    </Link>
   );
 };
 
